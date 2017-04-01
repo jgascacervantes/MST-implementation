c0a4520e1cc4da216850c8c3133f6581ba3bd7f4
Jorge <jgascacervantes@gmail.com>
2017-04-01 02:10:04 -0500

    add algorithm to main

--- main.cpp
+++ main.cpp
@@ -3,7 +3,9 @@
 #include "Graph.h"
 int find(int parent[], int i );
 void uni(int parent[], int x, int y);
-int detectCycle(Graph g);
+int detectCycle(const Graph& g);
+bool compareWeight(const Graph::Edge e1, const Graph::Edge e2);
+
 
 /*
  * implementation of Kruskal's MST algorithm using union-find.
@@ -15,16 +17,29 @@ int detectCycle(Graph g);
 int main() {
     Graph g(3, 3); //create a graph with 3 vertices and 3 edges
 
-    /*link the edges in a cyclic way*/
     g.edges.push_back(Graph::Edge(0,1, 12));
     g.edges.push_back(Graph::Edge(1,2, 2));
     g.edges.push_back(Graph::Edge(2,0, 6));
-
+    //sort edges by weight so we check the lightest ones first
+    std::sort(g.edges.begin(), g.edges.end(), compareWeight);
     /* MST representation*/
-    Graph MST = g;
+    Graph MST(3,1);
+    int i = 1;
+    MST.edges.push_back(g.edges[0]);
 
-    if(detectCycle(MST))
-        std::cout << "detected cycle" << std::endl;
+
+    /*a full MST has V-1 edges
+     *push an edge to our MST, check for cycle, pop if cycle detected
+     */
+    while(MST.e < g.v - 1){
+        MST.edges.push_back(g.edges[i]);
+        MST.e++;
+        if(detectCycle(MST)){
+            MST.edges.pop_back();
+            MST.e--;
+        }
+        i++;
+    }
 
     MST.printGraph();
     return 0;
@@ -39,19 +54,20 @@ int find(int parent[], int i) {
 }
 
 //unite two subsets
-void uni(int *parent, int x, int y) {
+void uni(int parent[], int x, int y) {
     int xset = find(parent, x);
     int yset = find(parent, y);
     parent[xset] = yset;
 }
 
-int detectCycle(Graph g) {
-    int parent[g.v];
-    std::fill_n(parent, g.v, -1);
+int detectCycle(const Graph& g) {
+    Graph gr = g;
+    int parent[gr.v];
+    std::fill(parent, parent + sizeof(parent), -1);
 
-    for(int i = 0; i < g.e; i++){
-        int x = find(parent, g.edges[i].src);
-        int y = find(parent, g.edges[i].dest);
+    for(int i = 0; i < gr.e; i++){
+        int x = find(parent, gr.edges[i].src);
+        int y = find(parent, gr.edges[i].dest);
 
         if (x == y)
             return 1;
@@ -61,4 +77,7 @@ int detectCycle(Graph g) {
     return 0;
 }
 
+bool compareWeight(const Graph::Edge e1, const Graph::Edge e2) {
+    return e1.weight < e2.weight;
+}
 
